e8dda1cfeeaad6df52a3e9de0116d7e95cd86678
useCamelCase <qwertyytofficialyt@gmail.com>
2021-09-05 10:47:17 -0700

    dependencies

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ShardingManager } = require('discord.js');
 require('dotenv').config()
 const manager = new ShardingManager('./bot/bot.js', { token: process.env.token });
 const { app } = require("./server/express")
+const login = require("./server/api/login")
+login.rand()
 app.get('/', (req, res) => {
     res.send("wip")
 })
--- server/api/login.js
+++ server/api/login.js
@@ -1,5 +1,7 @@
 const { app } = require("./server/express")
 const crypto = require('node:crypto')
-app.get('/api/rand', (req, res) => {
+module.exports = {
+rand = app.get('/api/rand', (req, res) => {
     res.send(crypto.randomBytes(32).toString("base64"))
-})
+}),
+}
