5b26fbf050feced100b93db42dca4e196d510e75
useCamelCase <qwertyytofficialyt@gmail.com>
2021-09-05 11:01:31 -0700

    dependencies

--- server/api/login.js
+++ server/api/login.js
@@ -1,5 +1,5 @@
 const { app } = require("../express")
-const crypto = require('node:crypto')
+const crypto = require('crypto')
 module.exports = {
     rand: () => app.get('/api/rand', (req, res) => {
         res.send(crypto.randomBytes(32).toString("base64"))
