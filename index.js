71e506aaaa6b162dd75040ade161231ca687f0b3
useCamelCase <qwertyytofficialyt@gmail.com>
2021-09-05 10:21:05 -0700

    stuff

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { ShardingManager } = require('discord.js');
 require('dotenv').config()
 const manager = new ShardingManager('./bot/bot.js', { token: process.env.token });
-
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 3000
+app.get('/', (req, res) => {
+    res.send("wip")
+})
+app.listen(port, () => {
+    console.log("web app started at " + port + "port")
+})
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
